4c30b818a748480d928f4a19a0643c5d38a52e93
Leye-ELhadji12 <86428272+Leye-ELhadji12@users.noreply.github.com>
2024-04-07 01:10:33 +0200

    Beginning application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./homePage/header/header.component";
+import {BannerComponent} from "./homePage/banner/banner.component";
+import {ContentComponent} from "./homePage/content/content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BannerComponent,
+    ContentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
